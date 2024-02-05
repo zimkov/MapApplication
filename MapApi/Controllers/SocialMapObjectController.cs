b447b0a427a84aeb04a1b6d2a133045f371d1aa6
zimkov <zimkov.leha@gmail.com>
2024-02-05 19:20:08 +0400

    Добавил библиотеку для работы с онтологиями
    Добавил библиотеку dotnetRDF, написал метод GET для извлечения триплетов при помощи sparql-запросов из онтологии

--- MapApi/Controllers/SocialMapObjectController.cs
+++ MapApi/Controllers/SocialMapObjectController.cs
@@ -3,6 +3,12 @@ using MapApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Web;
+using VDS.RDF;
+using VDS.RDF.Ontology;
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+using static Lucene.Net.Documents.Field;
 
 namespace MapApi.Controllers
 {
@@ -15,6 +21,42 @@ namespace MapApi.Controllers
         public SocialMapObjectController(ApplicationContext context)
         {
             _context = context;
+        }
+        [HttpGet]
+        [Route("api/SocialMapObject/getbyid")]
+        public string GetOntologyObjects()
+        {
+            //Create a graph to load into
+            IGraph g = new Graph();
+
+            //Load the OWL file
+            g.LoadFromFile("Ontology.owl");
+
+            string s = "Результат запроса: ";
+            
+
+            //First we need an instance of the SparqlQueryParser
+            SparqlQueryParser parser = new SparqlQueryParser();
+
+            //Then we can parse a SPARQL string into a query
+            SparqlQuery q = parser
+                .ParseFromString("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT ?subject ?object WHERE { ?subject rdfs:subClassOf ?object }");
+            Object results = g.ExecuteQuery(q);
+            if (results is SparqlResultSet)
+            {
+                //Print out the Results
+                SparqlResultSet rset = (SparqlResultSet)results;
+                foreach (SparqlResult result in rset)
+                {
+                    string decodedString = HttpUtility.UrlDecode(result.ToString());
+                    //int i = decodedString.IndexOf('#');
+                   // string iri = decodedString.Substring(i);
+                    Console.WriteLine(decodedString);
+                    Console.WriteLine("\n");
+                }
+            }
+
+            return s;
         }
         [HttpGet]
         public async Task<ActionResult<IEnumerable<SocialMapObject>>> GetSocialMapObject()
